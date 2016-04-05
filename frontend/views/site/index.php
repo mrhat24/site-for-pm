0614b511a150e34b6cbd966231be21eb4b43dcbc
mrhat <mrhat@10.220.134.66>
2016-04-05 14:34:36 +0700

    dsa

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -17,7 +17,7 @@ $this->title = 'ПМ';
 	<?php
 	echo Html::tag('h3','Добро пожаловать)!');                    
     ?>
-    here 42322
+    here 42322s
 	<!--
     <div class="jumbotron">
         <h1>Congratulations!</h1>
