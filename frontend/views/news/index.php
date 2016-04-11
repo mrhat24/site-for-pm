a147c58140f01acb3c14c74e446d256cfd1a7961
mrhat <mrhat@192.168.0.6>
2016-04-11 10:59:17 +0700

    add news

---
+++ frontend/views/news/index.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ListView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $searchModel frontend\models\NewsSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'News';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="news-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create News', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+<?php Pjax::begin(); ?>    <?= ListView::widget([
+        'dataProvider' => $dataProvider,
+        'itemOptions' => ['class' => 'item'],
+        'itemView' => function ($model, $key, $index, $widget) {
+            return Html::a(Html::encode($model->title), ['view', 'id' => $model->id]);
+        },
+    ]) ?>
+<?php Pjax::end(); ?></div>
